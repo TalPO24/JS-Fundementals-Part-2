b75e5798bfecc545f4e915f984dd31589d024908
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-03-14 18:50:35 +0200

    JS Fundementals

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function logger() {
     console.log('My name is Jonas')
 }
 
-// calling / running / invoking function
+//* calling / running / invoking function
 logger();
 logger();
 logger();
@@ -62,7 +62,6 @@ console.log(age3)
 const yearUntilRetirement = (birthYear, firstName) => {
     const age = 2037 - birthYear
     const retirement = 65 - age
-        // return retirement
     return `${firstName} retires in ${retirement} years`
 }
 
@@ -85,4 +84,26 @@ function fruitProccessor(apples, oranges) {
 }
 
 console.log(fruitProccessor(2, 3))
+*/
+
+//* REVIEWING FUCTIONS
+/*
+const calcAge = function(birthyear) {
+    return 2037 - birthyear
+}
+
+const yearUntilRetirement = function(birthYear, firstName) {
+    const age = calcAge(birthYear)
+    const retirement = 65 - age
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`)
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired`)
+        return -1
+    }
+}
+console.log(yearUntilRetirement(1991, 'Jonas'))
+console.log(yearUntilRetirement(1950, 'Mike'))
 */
