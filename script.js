d61d0217da7de0cbe8067d0e3aea22634278352f
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-03-14 14:44:10 +0200

    JS Fundementals

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //* ACTIVATING STRICT MODE 
+/*
 'use strict';
 let hasDriversLicense = false;
 const passTest = true;
@@ -8,3 +9,28 @@ if (hasDriversLicense) console.log('I can drive')
 
 // const interface = 'Audio'
 // const private = 543
+*/
+
+
+//* FUNCTIONS
+/*
+function logger() {
+    console.log('My name is Jonas')
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProccessor(apples, oranges) {
+    const juice = `juice with ${apples} apples and ${oranges} oranges.`
+    return juice
+}
+
+const appleJuice = fruitProccessor(5, 0)
+console.log(appleJuice)
+
+const appleOrangeJuice = fruitProccessor(2, 4)
+console.log(appleOrangeJuice)
+*/
