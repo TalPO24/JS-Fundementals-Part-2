03c70dd127024013d7fed0dbe3cb55135b910fca
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-03-15 11:52:57 +0200

    JS Fundementals

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ console.log(scoreD, scoreK)
 */
 
 //* INTRODUCTION TO ARRAYS
-
+/*
 const friend1 = "Michael"
 const friend2 = "Stevn"
 const friend3 = "Peter"
@@ -173,3 +173,38 @@ console.log(age1, age2, age3)
 
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])]
 console.log(ages)
+*/
+
+//* BASIC ARRAY OPERATIONS
+
+const friends = ["Michael", "Steven", "Peter"]
+
+//* ADD ELEMENTS
+const newLength = friends.push("jay")
+console.log(friends)
+console.log(newLength)
+
+friends.unshift('John')
+console.log(friends)
+
+
+//* REMOVE ELEMENTS
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped)
+console.log(friends)
+
+friends.shift(); // First
+console.log(friends)
+
+console.log(friends.indexOf('Steven'))
+console.log(friends.indexOf('Bob'))
+
+friends.push(23)
+console.log(friends.includes('Steven'))
+console.log(friends.includes('Bob'))
+console.log(friends.includes(23))
+
+if (friends.includes('Steven')) {
+    console.log(`you have a friend calld Steven`)
+}
