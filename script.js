5a3fdf4d86151cb46e61ef78d3857911f63e9c70
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-03-15 19:47:46 +0200

    JS Fundementals

--- script.js
+++ script.js
@@ -403,7 +403,7 @@ for (let i = 0; i < tal.length; i++) {
 */
 
 //* LOOPING BACKWARDS AND LOOPS IN LOOPS
-
+/*
 const tal = [
     "tal",
     "Poris",
@@ -423,3 +423,24 @@ for (let exercise = 1; exercise < 4; exercise++) {
         console.log(`exercise ${exercise} lifting weights repetition ${rep}🏋️`)
     }
 }
+*/
+
+//* TEH WHILE LOOP
+/*
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep} 🏋️`)
+}
+let rep = 1;
+while (rep <= 10) {
+    console.log(` WHILE:Lifting weights repetition ${rep} 🏋️`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`You rolld a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log('Loop is about to end ...')
+}
+*/
